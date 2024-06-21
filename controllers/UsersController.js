fd0cac4fab0ccbb72884a2834d3610d04f0c5bc1
himza 25 <himza.of.theking95@gmail.com>
2024-06-21 15:49:21 -0700

    update task

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
-import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -22,6 +23,26 @@ class UsersController {
     const newUser = result.ops[0];
 
     return res.status(201).json({ id: newUser._id, email: newUser.email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
