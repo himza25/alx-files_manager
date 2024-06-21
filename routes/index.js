7e362ea2ca928579628d658d0ce54d26113c117c
himza25 <himza.of.theking95@gmail.com>
2024-06-21 10:07:16 -0700

    task 2

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-// src/routes/index.js
+// routes/index.js
 import { Router } from 'express';
+import AppController from '../controllers/AppController';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.status(200).send({ message: 'Welcome to the Files Manager API' });
-});
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
 export default router;
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+// server.js
 import express from 'express';
 import bodyParser from 'body-parser';
-import routes from './routes/index'; // Remove the .js extension
+import routes from './routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api', routes);
+app.use('/', routes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
